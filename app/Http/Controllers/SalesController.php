72d5305c0f4fe3a16309811d73cd8ed940ba79a4
mgalal8866 <mgalal8866@gmail.com>
2022-10-28 17:58:52 +0200

    modified:   app/Http/Controllers/SalesController.php 	modified:   app/Models/sales.php 	modified:   resources/views/newsales.blade.php

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -2,58 +2,72 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\branch;
+use Carbon\Carbon;
 use App\Models\sales;
 use App\Models\times;
-use Carbon\Carbon;
+use App\Models\branch;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class SalesController extends Controller
 {
+    public function time_sale()
+    {
+        $times    = times::whereActive(0)->get();
+        $timenw   = Carbon::now()->format('H');
+
+        foreach ($times as $index => $time){
+            if( $timenw >= Carbon::parse($time->time_sales)->subHour(1)->format('H')&&
+                $timenw <= Carbon::parse($time->time_sales)->addHour(1)->format('H'))
+            {
+                $timesales=['id'=>$time->id,'timesales'=>$time->time_sales];
+                break;
+            }
+            else{
+                if ($times->count() == $index + 1){
+                    $timesales = null;
+                    break;
+                }
+            }
+        }
+        return $timesales??null;
+    }
 
     public function index()
     {
         $times = times::whereActive(0)->get();
         $branches = branch::whereActive(0)->get();
         $sales = sales::whereDate('created_at', Carbon::today())->get();
-    // dd($sales);
         return view('sales',compact('times','branches','sales'));
     }
+
     public function newsales()
     {
-        // $now = Carbon::parse('4:00 AM')->format('h:i A');
-
-
-        $times = times::whereActive(0)->get();
-        // $timeadd = Carbon::now()->addHour(2)->format('h:i A');
-        // $timesub = Carbon::now()->subHour(2)->format('h:i A');
-        $s = '05:00 PM';
-        $timeadd = Carbon::parse( $s )->addHour(1)->format('h:i A');
-        $timesub = Carbon::parse( $s)->subHour(1)->format('h:i A');
-       //  dd($timeadd,$timesub );
-        $branches = branch::whereActive(0)->get();
-        return view('newsales',compact('branches','times','timeadd','timesub'));
+        $branches   = branch::whereActive(0)->get();
+        $times      = times::whereActive(0)->get();
+        $timesales  = $this->time_sale();
+        return view('newsales',compact('branches','times','timesales'));
     }
 
     public function insertsales(Request $request)
     {
-        $times = times::whereActive(0)->get();
+        $Sales = sales::whereDate('created_at', Carbon::today())->where('time_id',$request->time_sales)->where('branch_id',$request->branch)->get();
+            if($Sales->count() > 0){
+
+                $request->session()->flash('alert-success', 'User was successful added!');
+                return Redirect::to('/new/sales');
+            }else{
+                sales::create([
+                        'sales'     => $request->time_sales?$request->sales:null,
+                        'time_id'   => $request->time_sales??null,
+                        'branch_id' => $request->branch,
+                        'close'     => $request->time_sales?null:$request->sales
+                    ]);
+                $request->session()->flash('alert-success', 'jl');
+                return Redirect::to('/new/sales');
+            }
 
 
-        $timenw = Carbon::now()->format('H');
-        $dd='';
-        foreach ($times as $time){
-            if($timenw >= Carbon::parse($time->time_sales)->subHour(1)->format('H')&&
-             $timenw  <= Carbon::parse($time->time_sales)->addHour(1)->format('H')  ){
-            $dd=$time->time_sales;
-           }
-        }
 
-        dd( $dd);
-
-
-        $request['time'] = 'sss';
-        dd($request->all());
-        return view('newsales',compact('branches','times','timeadd','timesub'));
     }
 }
--- resources/views/newsales.blade.php
+++ resources/views/newsales.blade.php
@@ -7,41 +7,75 @@
 @endsection
 
 @section('content')
-
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <span class="text-black  text-center fw-bold">  Selse </span>
-                    @foreach ($times as $time)
-                        @if (  \Carbon\Carbon::now()->format('H') >= \Carbon\Carbon::parse($time->time_sales)->subHour(1)->format('H') &&
-                               \Carbon\Carbon::now()->format('H') <= \Carbon\Carbon::parse($time->time_sales)->addHour(1)->format('H'))
-                              {{\Carbon\Carbon::parse($time->time_sales)->format('h:i A')}}
-                              @break
-                        @else
-                        @if ($times->count() == $loop->index +1)
-                        <span class="text-danger text-center fw-bold">No Houre Sales</span>
-
-                        @break
-                        @endif
-
-                        @endif
-                    @endforeach
-
+    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+        <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+            <path
+                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+        </symbol>
+        <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+            <path
+                d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+        </symbol>
+        <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+            <path
+                d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+        </symbol>
+    </svg>
+    <div class="flash-message">
+        @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+            @if (Session::has('alert-' . $msg))
+                <p class="alert alert-{{ $msg }} text-center d-flex align-items-center">
+                    {{ Session::get('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert"
+                        aria-label="close">&times;</a></p>
+            @endif
+        @endforeach
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card m-5">
+                <div class="card-header text-center">
+                    <span class="text-black  text-center fw-bold "> Selse </span>
+                    @if ($timesales != null)
+                        {{ $timesales['timesales'] }}
+                    @else
+                        <div class="alert alert-danger text-center d-flex align-items-center" role="alert">
+                            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img"
+                                aria-label="Danger:">
+                                <use xlink:href="#exclamation-triangle-fill" />
+                            </svg>
+                            <div>
+                                No Houre Sales / Close time
+                            </div>
+                        </div>
+                    @endif
                 </div>
                 <form action="/insert/sales" method="POST">
                     @csrf
-                        <div class="card-body">
-                                <select  class="form-select" name="branch">
-                                    <option selected >Choose Branch</option>
-                                    @foreach ( $branches as  $branch)
-                                    <option value="{{ $branch->id}}">  {{ $branch->name}} </option>
-                                    @endforeach
-                                </select>
+                    <div class="card-body">
+                        <input type="number" hidden name="time_sales"
+                            value="{{ $timesales != null ? $timesales['id'] : null }}">
+                        <div class="mb-3">
+                            <label for="selectbranch" class="form-label">Branch </label>
+                            <select class="form-select" id="selectbranch" name="branch" required>
+                                <option selected value="">Choose Branch</option>
+                                @foreach ($branches as $branch)
+                                    <option value="{{ $branch->id }}"> {{ $branch->name }} -
+                                        <span class="badge bg-info text-dark">Last: {{ $branch->close }} </span>
+                                    </option>
+                                @endforeach
+                            </select>
                         </div>
-                        <div class="card-footer">
-                        <button type="submit" class="btn btn-success">Send</button>
+                        <div class="mb-3">
+                            <label for="salestext" class="form-label">Sales</label>
+                            <input type="number" step="any" class="form-control" id="salestext" name="sales"
+                                placeholder="" required>
                         </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="d-grid gap-2">
+                            <button type="submit" class="btn btn-success btn-block">Send</button>
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
